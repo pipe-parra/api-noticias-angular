161d73929c206e6320c309821e1ee8723a39a44f
pipe <felipe.parra@blackmind.cl>
2022-05-22 21:36:13 -0400

    agrego listNoticias en el app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NoticiaService } from './services/noticia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apinoticias';
+  listNoticias: any[] = [];
 
+  constructor(private _noticiaService:NoticiaService){
+
+  }
 
   buscarNoticias(parametros:any){
     console.log("soy el padre");
     console.log(parametros);
+
+    this._noticiaService.getNoticias(parametros).subscribe(data =>{
+      console.log(data);
+      this.listNoticias = data.articles;
+    })
   }
 }
