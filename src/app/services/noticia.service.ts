793fb20acbfe2ec555e3f7d464a0fe2084a92760
pipe <felipe.parra@blackmind.cl>
2022-05-22 21:25:42 -0400

    terminamos el servicio

--- src/app/services/noticia.service.ts
+++ src/app/services/noticia.service.ts
@@ -1,13 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiaService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
-  getNoticias(parametros:any){
+  getNoticias(parametros:any):Observable<any>{
     const URL = 'https://newsapi.org/v2/top-headlines?country='+parametros.pais+'&category='+parametros.categoria+'&apiKey=a4bd01aa54b647eb929743654fc7d633';
+    return this.http.get(URL);
   }
 }
